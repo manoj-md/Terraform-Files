4a9bd4e81bd17dbdc6868b2f93a18f8bd6ec357c
manoj-md <86720825+manoj-md@users.noreply.github.com>
2021-06-30 18:56:46 +0530

    Delete main.tf

--- main.tf
+++
@@ -1,9 +0,0 @@
-module "dynamodb_table" {
-  source   = "./DynamoDB-Modules/"
-
- aws_dynamodb_table_name     = "demo-dynamodb-test"
- aws_dynamodb_table_hash_key = "PK"
- aws_dynamodb_table_range_key = "SK"
- aws_dynamodb_table_read_capacity = 500
- aws_dynamodb_table_write_capacity = 50
-}
